09f04d7b03e3f1bc1c02b55b5f73bd80049b1b07
Adam <adamgrharvey@gmail.com>
2022-07-07 13:33:02 -0700

    adds testing to our breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,27 @@
 const request = require('request');
 const apiKey = "2fbe7f12-b537-4040-a2a2-5ee222655364";
-const args = process.argv.slice(2);
 
-let options = { method: 'GET',
-  url: `https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`,
-  headers: { 'x-api-key': apiKey } };
+const fetchBreedDescription = function(breedName, callback) {
+  let options = { method: 'GET',
+    url: `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    headers: { 'x-api-key': apiKey } };
 
-request(options, function(error, response, body) {
-  if (error) throw new Error(error);
+  request(options, function(error, response, body) {
+    if (error) {
+      throw new Error(error);
+    }
+    
+    const resultObj = JSON.parse(body);
+    if (resultObj[0] === undefined) {
+      error = "No results found.";
+      callback(error, null);
+    } else {
+      callback(null, resultObj[0].description);
+    }
+    
+  });
+};
 
-  const resultObj = JSON.parse(body);
-  if (resultObj[0] === undefined) {
-    console.log();
-    throw new Error("Breed not found. Exiting...");
-    //exit();
-  }
-  console.log(resultObj[0].description);
-});
+module.exports = {
+  fetchBreedDescription
+};
